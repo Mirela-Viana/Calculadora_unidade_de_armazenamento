bdf982ea266902cdccc20a6af8c7213cc466da07
unknown <MirelaViana.Melo@gmail.com>
2023-03-23 16:34:09 -0300

    início e organização das informações de unidade de armazenamento

--- interface_linha_comando.py
+++ interface_linha_comando.py
@@ -1,4 +1,5 @@
-from todas_as_calculadoras import converterStringParaFloat, bitParaByte, ByteParabit
+from todas_as_calculadoras import converterStringParaFloat, bitParaByte, byteParaBit, byteparakilobyte,Kilobyteparamegabyte, kylobyteParabyte, megabyteParakilobyte, MegabyteParagigabyte, GigabyteParaMegabyte, terabyteparapetabyte, petabyteParaterabyte 
+
 print(' - escreva 1 para bitparaByte \n -Escreva 2 byteparabit')
 funcEscolha = input ()
 if(funcEscolha == '1'):
@@ -8,7 +9,53 @@ if(funcEscolha == '1'):
 
 elif(funcEscolha == '2'):
     entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
-    valorConvertido = ByteParabit (entradaDoTecladoValorASerConvertido)
+    valorConvertido = byteParaBit (entradaDoTecladoValorASerConvertido)
     print(valorConvertido) 
+
+elif(funcEscolha == '3'):
+    entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+    valorConvertido = byteparakilobyte (entradaDoTecladoValorASerConvertido)
+    print(valorConvertido) 
+
+elif(funcEscolha == '4'):
+    entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+    valorConvertido = kylobyteParabyte (entradaDoTecladoValorASerConvertido)
+    print(valorConvertido) 
+
+
+elif(funcEscolha == '5'):
+    entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+    valorConvertido = Kilobyteparamegabyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido) 
+
+elif(funcEscolha == '6'):
+    entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+    valorConvertido = megabyteParakilobyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido) 
+
+elif(funcEscolha == '7'):
+    entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+    valorConvertido = MegabyteParagigabyte (entradaDoTecladoValorASerConvertido)
+    print(valorConvertido) 
+
+elif(funcEscolha == '8'):
+    entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+    valorConvertido = GigabyteParaMegabyte(entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif(funcEscolha == '9'):
+    entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+    valorConvertido = terabyteparapetabyte (entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+elif(funcEscolha == '10'):
+    entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+    valorConvertido = petabyteParaterabyte (entradaDoTecladoValorASerConvertido)
+    print(valorConvertido)
+
+
+
+
+
 
     
