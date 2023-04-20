56e94419f2c80e1d92e8b122773d3fb254e96711
unknown <MirelaViana.Melo@gmail.com>
2023-04-20 14:26:54 -0300

    Criação das variáveis + add de lista e funcionamento inicial do teclado.

---
+++ avaliação.py
@@ -0,0 +1,29 @@
+print( 'Valor padrão de conversão: 1024')
+valor1 = 1024 
+Unidade1 = ['Bit', 'Byte', 'MG', 'GB', 'TB', 'PB']
+valorinicial = int(input('insira um valor:'))  
+
+uni1 =(input('insira a unidade de armazenamento 1:'))
+while not uni1 in Unidade1:
+    uni1 = (input('insira a unidade de armazenamento 1:'))
+
+uni2 = (input('insira a unidade de armazenamento 2:'))
+while not uni2 in Unidade1:
+    uni2 = (input('insira a unidade de armazenamento 2:'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
